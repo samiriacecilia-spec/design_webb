594cbd759be7f4f224e408e6af4af6cc6b1c525b
samiriacecilia-spec <samiria.cecilia@escolar.ifrn.edu.br>
2026-08-20 13:25:25 -0300

    Atividade concluida

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,14 @@
 alert("Olá mundo!");
+
+var nasc = 1991;
+let nome = "Pedrina";
+const viva = true;
+let altura = 1.64;
+
+if (viva){
+    let saudacao = "Olá"+ nome +"!";
+    let msg = `Altura ${altura}m | Idade ${2026-nasc}`;
+    alert(saudacao+"\n"+msg);
+}else{
+    alert("R.I.P");
+}
