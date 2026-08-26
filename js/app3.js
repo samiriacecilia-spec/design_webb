96cc19936ccefbdb6636c6df5c2451c33bd9ae18
samiriacecilia-spec <samiria.cecilia@escolar.ifrn.edu.br>
2026-08-26 17:56:26 -0300

    app3

---
+++ js/app3.js
@@ -0,0 +1,15 @@
+let nasc = 2009;
+let nome = "Samiria";
+let viva = true;
+let altura = 1.64;
+let sexo;
+let peso = null;
+let imc = "peso"*10;
+
+alert(`Ano de nascimento - tipo: ${typeof(nasc)}`)
+alert(`Nome - tipo: ${typeof(nasc)}`)
+alert(`Viva - tipo: ${typeof(nasc)}`)
+alert(`Altura - tipo: ${typeof(nasc)}`)
+alert(`Sexo - tipo: ${typeof(nasc)}`)
+alert(`Peso - tipo: ${typeof(nasc)}`)
+alert(`IMC - tipo: ${typeof(nasc)}`)
